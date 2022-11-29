3a146360c467d7b9efe0783f18a9741586de0a71
Luisa-Inc <118922382+Luisa-Inc@users.noreply.github.com>
2022-11-29 09:39:28 +0100

    Update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'pasta';
-document.write(favoriteFood);
+let name = 'John';
+let age = 32;
+let message = `Hello!
+This is a longer message here.
+My name is: ${name},
+and I'm ${age}.`;
