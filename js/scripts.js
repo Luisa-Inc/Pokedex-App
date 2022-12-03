3d613532da99024cc852e9bf9bb2e59865ba6306
Luisa-Inc <118922382+Luisa-Inc@users.noreply.github.com>
2022-12-03 12:29:08 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-/*let pokemonList = [
+let pokemonList = [
     
   { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
   { name: 'Charmander', height: 0.6, type: ['fire']},
   { name: 'Pikachu', height: 0.4, type: ['electric']},
 
-]*/
+]
 
 
 // pokemon list 'for each' function
